366a318944f0f59edea5525654663ba6e34e6c48
Vignesh1399 <30726159+Vignesh1399@users.noreply.github.com>
2017-08-13 19:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,9 @@ button.onclick=function(){
     };
     
    //Make the request
-   
-   request.open('GET','http://vigneshrc1999.imad.hasura-app.io/submit-name?name='+name,true);
+    var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+   request.open('GET','http://vigneshrc1999.imad.hasura-app.io/counter',true);
    request.send(null);
 };
 //Submit name
@@ -37,18 +38,37 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     //Make a request to the server and send the name
+    var request=new XMLHttpRequest();
     
     
-    //Capture a list of names and render it as a list
-    var names=['name1','name2','name3','name4'];
+    //Capture the response and store it in a variable
+    
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
     var list='';
   for(var i=0;i<names.length;i++){
       list+='<li>'+names[i]+'</li>';
   }  
   var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
+  ul.innerHTML=list; 
+            }
+        }
+    };
     
+   //Make the request
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+   request.open('GET','http://vigneshrc1999.imad.hasura-app.io/submit-name?name='+name,true);
+   request.send(null);
     
+    //Capture a list of names and render it as a list
+
+   
     
     
 };
