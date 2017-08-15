c637d63f9a32d16b31c8a24821e9b4c9cb909e89
Vignesh1399 <30726159+Vignesh1399@users.noreply.github.com>
2017-08-15 18:48:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/articles/:articleName',function(req,res){
            
        } 
     });
-   res.send(createTemplate(articleData));
+   
 });
 
 app.get('/ui/style.css', function (req, res) {
