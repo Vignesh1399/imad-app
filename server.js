d98e8335ae99ffbfce3cafac7e7d8c745e4801dc
Vignesh1399 <30726159+Vignesh1399@users.noreply.github.com>
2017-08-15 18:35:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,9 +122,22 @@ app.get('/submit-name/',function(req,res){
 });
 
 
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName',function(req,res){
+   
+    
+    pool.query("SELECT * FROM article WHERE title= " + req.params.articleName,function(err,result){
+       if(err){
+           res.status(500).send(err,toString());
+       }
+        else{
+            if(result.rows.length===0) {
+             var articleData=result.rows[0];
+             res.send(createTemplate(articleData));
+            }
+           
+       } 
+    });
+   res.send(createTemplate(articleData));
 });
 
 app.get('/ui/style.css', function (req, res) {
