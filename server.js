7ec0d4a229f1362a72f1a0d4475a1a8eeb770e2f
Vignesh1399 <30726159+Vignesh1399@users.noreply.github.com>
2017-08-15 14:53:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user:'vigneshrc1999',
+    database:'vigneshrc1999',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -87,6 +96,18 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    POOL.query('SELECT * from test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result));
+       } 
+    });
+});
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter++;
